73857c6e5a260c91cac2e8f4ae389ad5085dcd04
Arif Yazici <arifayrca@example.com>
2026-07-30 02:08:11 +0300

    hw3-crud: contact create/update/delete, error handling

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 export const setupServer = () => {
   const app = express();
@@ -11,36 +13,11 @@ export const setupServer = () => {
   app.use(cors());
   app.use(pino({ transport: { target: 'pino-pretty' } }));
 
-  // GET /contacts
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use(contactsRouter);
 
-  // GET /contacts/:contactId
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+  app.use(notFoundHandler);
 
-    if (!contact) {
-      return res.status(404).json({ message: 'Contact not found' });
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  // 404 Handler (Buradaki '*' hatasını düzelttim)
-  app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
-  });
+  app.use(errorHandler);
 
   const PORT = Number(getEnvVar('PORT', '3000'));
   app.listen(PORT, () => {
