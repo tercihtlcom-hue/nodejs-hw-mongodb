90321cb9b15ba7fea4bb8e0aa6cf4d24ccbb0a28
Arif Yazici <tercihtl.com@gmail.com>
2026-07-29 06:03:25 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,14 @@
 import { Contact } from '../db/models/contact.js';
+import mongoose from 'mongoose';
 
 export const getAllContacts = async () => {
   return await Contact.find();
 };
 
 export const getContactById = async (contactId) => {
+  // Geçersiz ID kontrolü
+  if (!mongoose.Types.ObjectId.isValid(contactId)) {
+    return null;
+  }
   return await Contact.findById(contactId);
 };
