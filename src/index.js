ed566f0da9a624dcaca0fa21a5f2808b0ffb3cde
Arif Yazici <tercihtl.com@gmail.com>
2026-07-30 19:40:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import { initMongoConnection } from './db/initMongoConnection.js';
 
 const bootstrap = async () => {
   try {
-    // Önce veritabanına bağlanıyoruz
     await initMongoConnection();
-    // Bağlantı başarılıysa server'ı ayağa kaldırıyoruz
     setupServer();
   } catch (error) {
     console.error('Bootstrap error:', error);
