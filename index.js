b0fdabb2318776aaa14494efa53f5e69539dceaf
Arif Yazici <tercihtl.com@gmail.com>
2026-08-04 17:48:00 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import { initMongoConnection } from './db/initMongoConnection.js';
-import { setupServer } from './server.js';
-
-initMongoConnection().then(() => {
-  setupServer();
-});
