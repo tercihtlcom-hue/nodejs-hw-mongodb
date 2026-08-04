55e7be9e50b4938df3aba1b1e16206f53ea5c3fd
Arif Yazici <tercihtl.com@gmail.com>
2026-08-04 20:30:06 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+import contactsRouter from './routes/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(pino());
+
+  app.use(contactsRouter);
+
+  app.use(notFoundHandler);
+  app.use(errorHandler);
+
+  const port = Number(process.env.PORT) || 3000;
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+};
