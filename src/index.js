61fa0ad2c1544244bb2cd3cda852eb2bad77dd40
Arif Yazici <tercihtl.com@gmail.com>
2026-08-04 18:33:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import 'dotenv/config'; // MUTLAKA EN ÜSTTE OLMALI!
+import 'dotenv/config';
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
 const bootstrap = async () => {
   try {
-    // Önce veritabanına bağlanıyoruz
     await initMongoConnection();
-    // Bağlantı başarılıysa server'ı ayağa kaldırıyoruz
     setupServer();
   } catch (error) {
     console.error('Bootstrap error:', error);
