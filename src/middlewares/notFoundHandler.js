f035a5c0f5212c1a29a21bb21f9d0a88adf098f8
Arif Yazici <arifayrca@example.com>
2026-08-05 02:03:06 +0300

    hw4: clean up duplicate folders, keep single clean structure

--- src/middlewares/notFoundHandler.js
+++
@@ -1,5 +0,0 @@
-import createHttpError from 'http-errors';
-
-export const notFoundHandler = (req, res, next) => {
-  next(createHttpError(404, 'Route not found'));
-};
--- src/utils/getEnvVar.js
+++
@@ -1,6 +0,0 @@
-export const getEnvVar = (name, defaultValue) => {
-  const value = process.env[name];
-  if (value) return value;
-  if (defaultValue) return defaultValue;
-  throw new Error(`Missing: process.env['${name}'].`);
-};
